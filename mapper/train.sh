58c0413f94fc3207cedf0af573334a87de748bd1
yingchen001 <yingchen001@e.ntu.edu.sg>
2022-07-06 14:14:38 +0800

    modified training example

--- mapper/train.sh
+++ mapper/train.sh
@@ -1,6 +1,7 @@
 # for dog/cat
 DESCRIPTION=$1
 python scripts/train.py --exp_dir ../results/${DESCRIPTION// /_}  --description "$DESCRIPTION" \
+--source_text "dog" \
 --stylegan_size 512 --w_space --id_lambda 0 --perc_lambda 0.02 \
 --latents_train_path ../latent_codes/afhqdog/W_afhqdog_train.pt \
 --latents_test_path ../latent_codes/afhqdog/W_afhqdog_test.pt \
